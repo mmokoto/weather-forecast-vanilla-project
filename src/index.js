11dfc39ff1d9cf195f1168abb96fc8a0533a8792
mmokoto <miranda.mokoto@outlook.com>
2024-05-05 14:22:57 +0200

    added windspeed and humidity elements

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ function refreshWeather(response) {
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
 
-    console.log(response.data.condition.description);
+    
 
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 
 }
