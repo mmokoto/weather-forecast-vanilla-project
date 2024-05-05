e92e8e43a242e2077d66486dcf15e983c3036a94
mmokoto <miranda.mokoto@outlook.com>
2024-05-05 11:52:36 +0200

    updated the apikey,url and search info

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+    console.log(response.data);
+}
+
+
+function searchCity(city) {
+    let apiKey = "3a790fcd8fct145b403a980of3a9b38b";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+    axios.get(apiUrl).then(refreshWeather);
+
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
