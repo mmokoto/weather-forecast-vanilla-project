c4429313bf2fd6f010be057c12ef2181567d4221
mmokoto <miranda.mokoto@outlook.com>
2024-05-05 12:01:35 +0200

    edited apiUrl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function refreshWeather(response) {
-    console.log(response.data);
+    console.log(response.data.Temperature.current);
 }
 
 
 function searchCity(city) {
     let apiKey = "3a790fcd8fct145b403a980of3a9b38b";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
     axios.get(apiUrl).then(refreshWeather);
 
 }
